19e3535e27b49db299edb22edfa5d7deac6d223e
Pajsen9263 <42203569+Pajsen9263@users.noreply.github.com>
2021-09-07 20:58:33 +0200

    Update example.js

--- commands/example.js
+++ commands/example.js
@@ -4,6 +4,7 @@ module.exports = {
     options: [ // Types: 1, 2, 3, 4, 5, 6, 7
         // If you want to know which option the type number is equal to, check the github page.
         // https://github.com/iClaudette/discord.js-v13-slash-bot#for-developers
+        {type: 4, name: "Usless or not", description: "Set a required field", required: true},
         { type: 1, name: 'user', description: 'Mention user.' },
         { type: 3, name: 'channel', description: 'Mention channel.' }
     ],
